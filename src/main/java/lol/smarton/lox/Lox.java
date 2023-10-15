e5ee2c0cf67c308d32c7296238bbf5c7ea97fe59
Alexandru-Sergiu Marton <brown121407@posteo.ro>
2023-10-15 10:14:00 +0300

    4. Scanning
    With nested block comments

---
+++ src/main/java/lol/smarton/lox/Lox.java
@@ -0,0 +1,69 @@
+package lol.smarton.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalTime;
+
+import static java.lang.StringTemplate.STR;
+
+public class Lox {
+    static boolean hadError = true;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        var source = Files.readString(Paths.get(path));
+        run(source);
+
+        if (hadError) {
+            System.exit(65);
+        }
+    }
+
+    private static void runPrompt() throws IOException {
+        var inputStreamReader = new InputStreamReader(System.in);
+        var reader = new BufferedReader(inputStreamReader);
+
+        while (true) {
+            System.out.println("> ");
+            var line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        var scanner = new Scanner(source);
+        var tokens = scanner.scanTokens();
+
+        for (var token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    public static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.out.println(
+            STR."[line \{line}] Error \{where}: \{message}"
+        );
+        hadError = true;
+    }
+}
